0aad7b3372e809d61bbadcdd2cfaf85bc8c11f06
wyf-ACCEPT <define_wifi@163.com>
2022-05-07 15:11:38 +0800

    Add comments in the code

--- query.sql
+++ query.sql
@@ -3,7 +3,7 @@ WITH candidate_address AS (
     FROM ethereum."transactions"
     WHERE "to" = '\x3666f603Cc164936C1b87e207F36BEBa4AC5f18a' -- Hop Protocol: USDC Bridge (on Ethereum)
         AND "block_time" < now() - interval '37 days'
-        AND "block_time" > now() - interval '6 months'
+        AND "block_time" > now() - interval '6 months'        -- Too long time interval is difficult for Dune Analytic...
 ), candidate_sybil_transfer AS ( 
     SELECT "from", "to", "block_time", "hash"
     FROM ethereum."transactions" e RIGHT JOIN candidate_address c2
@@ -11,5 +11,5 @@ WITH candidate_address AS (
     WHERE "block_time" < now() - interval '37 days'
 )
 SELECT *
-FROM candidate_sybil_transfer
+FROM candidate_sybil_transfer                                 -- Note: this is only part of the statistical data, not the full version. Because the time range are limited, and the addresses are only those who interactived with "Hop USDC Bridge".
 WHERE "from" != "to"
